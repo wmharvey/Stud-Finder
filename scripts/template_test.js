ffd406a50c247cf74d22e2809820e4a5950de00f
Whitney Harvey <wmelrose01@gmail.com>
2015-12-17 09:39:00 -0800

    Add ancestry logic

--- scripts/template_test.js
+++ scripts/template_test.js
@@ -1,3 +1,22 @@
+var horseArray = [];
+
 function getJSON () {
-  
+  $.getJSON('data/horses.json', function(data) {
+    horseArray = data;
+    initTemplate();
+  })
 }
+
+function initTemplate() {
+  $.get('templates/showcase.html', function(data) {
+    var template = Handlebars.compile(data);
+    horseArray.forEach(function(item) {
+      if (item.showcase) {
+        var compiledHtml = template(item);
+        $('#insert_here').append(compiledHtml);
+      }
+    })
+  })
+}
+
+getJSON();
