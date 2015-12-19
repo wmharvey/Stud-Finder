15e774d7265ea7a0e450a835428548292d5b0ca7
Erik Gomez <egoxiv@gmail.com>
2015-12-18 16:27:12 -0800

    Adding toggle features

--- scripts/table_dropdown.js
+++ scripts/table_dropdown.js
@@ -1,10 +1,7 @@
-//alert("im working!");
 function toggleTable() {
   var tableContainer = $(this).closest('.container').find('.table_container');
   $('.toggle').on('click', function() {
     var table = $(this).closest('.container').find('.table_container').toggle();
-    console.log(table);
-    //table[0].toggle();
   })
 }
  function tableFilter() {
