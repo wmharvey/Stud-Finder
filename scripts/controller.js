5776c74cbe14e331a200ccc543f2841c4b328e83
Whitney Harvey <wmelrose01@gmail.com>
2015-12-18 17:04:00 -0800

    delete old files

--- scripts/controller.js
+++
@@ -1,6 +0,0 @@
-//contains the origin JSON data, an array of horse objects
-var horseArray = [];
-//an array of arrays, each array an ancestry tree
-var ancestryArray = [];
-//an array to create an ancestry tree, reusable
-var singleAncestry = [];
