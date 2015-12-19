b9b986534315cd31ac08b5c026ea88b43771460d
Laurie Brown <ltbrown@mac.com>
2015-12-19 12:32:53 -0800

    reorganizes structure, replaces shell.html with index.hml and consolidates the css files and implement pages at the bottom of index.html

--- _js/cw.js
+++ _js/cw.js
@@ -1,63 +1,61 @@
 var cw = cw || {};
-cw.horseArray = [];
+// TODO: figure out how to best handle namespacing, global scope issues
+//cw.horseArray = [];
+// in global scope?
+var horseArray = [];
 
 cw.loadSite = function() {
 	//common initiallogic on all pages
 	console.log("kicks off site");
-	cw.makeHandlebars();
+	//cw.makeHandlebars();
 };
 
 cw.getJSON = function() {
   $.getJSON('data/horses.json', function(data) {
-    cw.horseArray = data;
+    horseArray = data;
     cw.initTemplate();
+    cw.initGallery();
   })
 }
 
 cw.initTemplate = function() {
   $.get('templates/showcase.html', function(data) {
     var template = Handlebars.compile(data);
-    cw.horseArray.forEach(function(item) {
+    horseArray.forEach(function(item) {
       if (item.showcase) {
         var compiledHtml = template(item);
-        $('#insert_here').append(compiledHtml);
+        $('#insert_showcase').append(compiledHtml);
+        if (item.table) {
+          singleAncestry = [];
+          var indexArray = createAncestry(item, 1);
+          var table = createTable(indexArray);
+          $('#insert_placeholder').append(table);
+          $('#insert_placeholder').removeAttr('id');
+        } else {
+          $('#insert_placeholder').removeAttr('id');
+        }
       }
-    })
+    });
+    tableFilter();
+    toggleTable();
   })
 }
 
-cw.makeHandlebars = function(){
-	// var template = $('#entry-template').html();
-	var template = $('#thumb-template').html();
-	// Handlebars compiles the template into a callable function
-	var renderer = Handlebars.compile(template);
-	// put data in a variable
-	var myData = [{
-		imgAvatar 	: "_img/horses/135x135_poe.jpg",
-		videoPath	: "http://treehouse-code-samples.s3.amazonaws.com/html-video-and-audio/bridge.mp4",
-		name: "C-W’s Pride Over Easy (Poe)",
-		nickname: "poe",
-		id: "0007836",
-		img: "_img/576x300_poe.jpg",
-		description: "Foaled on May 25, 2000, Poe is a wonderful stallion. His personality and nature is like none other. He is smart, willing, and athletic. Most folks who come to the barn don&rsquo;t realize that he is a stallion because he is so gentle. Poe has a strong, compact, baroque body style, adding to his appeal and athleticism. Poe has a natural gait and good hard feet which allows him to remain barefoot year &rsquo;round. He gives his offspring his easy-going nature and trainability. His babies have also all been very personable and like to be with people."
-	},
-	{
-		imgAvatar 	: "_img/horses/135x135_poe.jpg",
-		videoPath	: "http://treehouse-code-samples.s3.amazonaws.com/html-video-and-audio/bridge.mp4",
-		name: "C-W&rsquo;s Pride Over Easy (Poe)",
-		nickname: "poe",
-		id: "0007836",
-		img: "_img/576x300_poe.jpg",
-		description: "Foaled on May 25, 2000, Poe is a wonderful stallion. His personality and nature is like none other. He is smart, willing, and athletic. Most folks who come to the barn don&rsquo;t realize that he is a stallion because he is so gentle. Poe has a strong, compact, baroque body style, adding to his appeal and athleticism. Poe has a natural gait and good hard feet which allows him to remain barefoot year &rsquo;round. He gives his offspring his easy-going nature and trainability. His babies have also all been very personable and like to be with people."
-	}];
-
-	// var showcases = myData;
-	var thumb = myData;
-	// call the compiled function with the template date
-	// var result = renderer({showcases});
-	var result = renderer({thumb});
-	$('#insert_gallery').append(result);
-	// $('#insert_showcase').append(result);
+cw.initGallery = function() {
+	$.get('templates/gallery.html', function(data) {
+    var template = Handlebars.compile(data);
+    horseArray.forEach(function(item) {
+      if (item.img_thumbnail) {
+        var compiledHtml = template(item);
+        $('#insert_gallery').append(compiledHtml);
+      }
+    })
+    playButton();
+  })
 }
+
+// KICKS THINGS OFF HERE!
+cw.getJSON();
+
 
 
