df03335ba664990df0817e49d5cb09644221092d
a-she <al@alswebdesigns.net>
2015-12-21 11:52:00 -0800

    Prep for Heroku deployment

---
+++ server.js
@@ -0,0 +1,14 @@
+var requestProxy = require('express-request-proxy');
+var express = require('express'),
+    port = process.env.PORT || 3000,
+    app = express();
+
+app.use(express.static('./'));
+
+app.get('*', function(request, response) {
+  response.sendFile('index.html', { root: '.' });
+});
+
+app.listen(port, function() {
+  console.log('Server started on port ' + port + '!');
+});
