6f62d7b203a4889838fc0bf62fc72fd0246cc397
Whitney Harvey <wmelrose01@gmail.com>
2015-12-22 12:42:09 -0800

    responsive sizing for lightbox videos

--- scripts/video.js
+++ scripts/video.js
@@ -1,21 +1,40 @@
-var myvid;
+  // function playButton() {
+  //   $(".play-button").on("click", function () {
+  //     console.log('click');
+  //     var $gallery = $("#insert_gallery");
+  //     var $video = $(".video");
+  //     var $returnButton = $(".return-button");
+  //     $gallery.hide();
+  //     $('html, body').animate({
+  //       scrollTop: $('#video_scroll').offset().top
+  //     }, 300, function() {
+  //       $video.fadeIn(600);
+  //     });
+  //
+  //     $returnButton.on("click", function(){
+  //       $gallery.show();
+  //       $video.hide();
+  //     });
+  //   })
+  // };
 
-  function playButton() {
-    $(".play-button").on("click", function () {
-      console.log('click');
-      var $gallery = $("#insert_gallery");
-      var $video = $(".video");
-      var $returnButton = $(".return-button");
-      $gallery.hide();
-      $('html, body').animate({
-        scrollTop: $('#video_scroll').offset().top
-      }, 300, function() {
-        $video.fadeIn(600);
-      });
+function iFrameSizing() {
+  $('.thumbnail').on('click', function() {
+    var width = '';
+    width += ($(window).width() - 50);
+    var height = '';
+    height += $(window).width() * (3/4);
+    $('iframe').css('width', width);
+    $('iframe').css('height', height);
 
-      $returnButton.on("click", function(){
-        $gallery.show();
-        $video.hide();
-      });
+    $(window).resize(function() {
+      var width = '';
+      width += ($(window).width() - 30);
+      var height = '';
+      height += $(window).width() * (3/4);
+      $('iframe').css('width', width);
+      $('iframe').css('height', height);
     })
-  };
+
+    })
+};
